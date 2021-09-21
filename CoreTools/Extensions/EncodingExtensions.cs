603cde8c9a7d4e47f9e3ef80d9c33a4df7bd3dd5
Cesar PV <cesar.pv27@gmail.com>
2021-09-21 00:33:52 -0400

    EncodingExtensions class added with extension methods to enconde and decode into base64 enconding.

---
+++ CoreTools/Extensions/EncodingExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+using ExThrower = CoreTools.Throws.ExceptionThrower;
+
+namespace CoreTools.Extensions
+{
+    public static class EncodingExtensions
+    {
+        public static string EncodeToBase64String(this Encoding encoding, string s)
+        {
+            ExThrower.ST_ThrowIfArgumentIsNullOrEmpty(s, nameof(s), nameof(s));
+
+            return Convert.ToBase64String(encoding.GetBytes(s));
+        }
+
+        public static string DecodeFromBase64String(this Encoding encoding, string s)
+        {
+            ExThrower.ST_ThrowIfArgumentIsNullOrEmpty(s, nameof(s), nameof(s));
+
+            return encoding.GetString(Convert.FromBase64String(s));
+        }
+    }
+}
